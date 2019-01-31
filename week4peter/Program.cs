e1afde58ef15330927f37c9b870c047eaf29ccaf
visualstudio <visualstudio@230-PC39>
2019-01-31 14:19:32 -0500

    change 2

--- week4peter/Program.cs
+++ week4peter/Program.cs
@@ -9,4 +9,45 @@ namespace week4peter
             Console.WriteLine("Hello World!");
         }
     }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+    }
+    class Countryside
+    {
+        public Village maple;
+        public Village Toronto;
+        public Village Ajax;
+
+        public void MapInitializer()
+        {
+            maple = new Village();
+            maple.VillageName = "Maple";
+            maple.previousVillage = null;
+            maple.nextVillage = Toronto;
+            Toronto = new Village();
+            Toronto.nextVillage = Ajax;
+            Ajax = new Village();
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+            Ajax.isAstrildeHere = true;
+
+
+        }
+
+        public void LookForAstildeHere
+        {
+
+        }
+    }
 }
